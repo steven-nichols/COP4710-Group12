029cb0033eb06b10a73c3421b9d7d56f3b31c17e
Kevin E. Porter <kevin.porter@knights.ucf.edu>
2011-04-07 02:48:58 -0400

    Created a simple login_fail page.

---
+++ views/login_fail.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta HTTP-EQUIV="REFRESH" content="3; url=<?php echo site_url(); ?>">
+</head>
+<body>
+
+<p>Your name/password pair was not recognized.</p>
+<P>Please try again.
+
+</body>
+</html>
