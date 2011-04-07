89987d5bb8703bee4b09b1841dbeedc3cc7ff051
Kevin E. Porter <kevin.porter@knights.ucf.edu>
2011-04-07 17:44:35 -0400

    Filled out the remaining empty stubs.

--- models/item_model.php
+++ models/item_model.php
@@ -37,12 +37,19 @@ class User_model extends CI_Model {
      * \param $type The type of item to search for.  Current values are listed 
      *        as constants at the top of the file.  Check the database for new 
      *        additions.
+     * \param $notavail Boolean, indicates if we want to return inactive items.  
+     *        If omitted, FALSE is assumed,
      *
      * \return All found items in an array or null on failure.
      */
-    function get_items_by_type($type)
+    function get_items_by_type($type, $notavail=0)
     {
-        $sql = "SELECT * From `items` WHERE `type` = ?";
+        if($notavail){
+            $sql = "SELECT * From `items` WHERE `type` = ?";
+        } else {
+            $sql = "SELECT * From `items` WHERE `type` = ? AND available = 1";
+        }
+
         $query = $this->db->query($sql, array($type));
         
         if($query->num_rows() > 0){
@@ -54,11 +61,23 @@ class User_model extends CI_Model {
     
     /**
      * Return a count of all items in the database.
+     * 
+     * \param $notavail Boolean indicating if we want to return information for 
+     *        unavailable items.  FALSE is assumed if omitted.
      *
-     * \return See above.
+     * \return See description.
      */
-    function get_item_count()
+    function get_item_count($notavail=0)
     {
+        if($notavail){
+            $sql = "SELECT COUNT(*) FROM `items`";
+        } else {
+            $sql = "SELECT COUNT(*) FROM `items` WHERE `available = 1";
+        }
+        
+        $query = $this->db->query($sql);
+        
+        return $query->row_array();
     }
     
     /**
@@ -68,11 +87,26 @@ class User_model extends CI_Model {
      * \param $start The offset from the start of the list from which the 
      *        returned items are taken.
      * \param $count The number of items to return
+     * \param $notavail Boolean indicating if we should return unavailable 
+     *        items.  Assumed FALSE if omitted.
      *
      * \return The sorted range of items as an array, null on failure.
      */
-    function get_item_range($start, $count)
+    function get_item_range($start, $count, $notavail=0)
     {
+        if($notavail){
+            $sql = "SELECT * FROM `items` ORDER BY `itemID` LIMIT ?,  ?";
+        } else {
+            $sql = "SELECT * FROM `items` WHERE `available` = 1 ORDER BY 
+                `itemID` LIMIT ?, ?";
+        }
+        
+        $query = $this->db->query($sql, array($start=0, $count=30));
+
+        if($query->num_rows > 0)
+        return $query->result_array();
+
+        return null;
     }
     
     /**
@@ -88,19 +122,29 @@ class User_model extends CI_Model {
      *        additions.
      * \param $available Boolean indicating if the item is available for 
      *        purchase in the CBCKids store.
-     * \param $picture The filename of the item's picture.  If left empty, the 
-     *        field will be set to the default value, "defaultitem.jpg"
      * \param $realcost The cost of the item in real money.  Decimal as xx.xx
      * \param $pointcost The cost of the item in CBCBucks. Integer values only.
      * \param $qty The current quantity of the item in inventory.
      * \param $minqty The quantity at which to warn the store supervisor to 
-     *        purchase more of this item. Defaults to 0 when left blank.
+     *        purchase more of this item.  If this is unknown or undesired, 
+     *        just set it to 0.
+     * \param $picture The filename of the item's picture.  If the name isn't 
+     *        known, either set this to "defaultitem.jpg" or omit it from the 
+     *        function call.
      *
      * \return TRUE/FALSE on success/failure
      */
     function add_item($description, $supplier, $url, $partno, $type, 
-        $available, $picture, $realcost, $pointcost, $qty, $minqty)
+        $available, $realcost, $pointcost, $qty, $minqty, $picture=
+        'defaultitem.jpg')
     {
+        $sql = "INSERT INTO `items` (`description`, `supplier`, `supplierurl`, 
+            `partno`, `type`, `available`, `picture`, `realcost`, `pointcost`, 
+            `qty`, `minqty`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        
+       return $query = $this->db->query($sql, array($description, $supplier, 
+        $url, $partno, $type, $available, $picture, $realcost, $pointcost, 
+        $qty, $minqty));
     }
     
     /**
@@ -115,18 +159,26 @@ class User_model extends CI_Model {
      * \param $partno The new or retained supplier's part number
      * \param $type The new or retained item type
      * \param $available The new or retained availability
-     * \param $picture The new or retained picture filename.  Leaving blank
-     *        resets picture to the default
      * \param $realcost The new or retained real cost
      * \param $pointcost The new or retained CBCBucks cost
      * \param $minqty The new or retained warning quantity
+     * \param $picture The new or retained picture filename.  Omit from the 
+     *        function call to reset the value to the default.
      *
      * \return TRUE/FALSE on success/failure
      */
     function modify_item($itemID, $description, $supplier, $url, $partno, 
-        $type, $available, $picture, $realcost, $pointcost, $minqty)
+        $type, $available, $realcost, $pointcost, $minqty, $picture=
+        'defaultitem.jpg')
     {
-    //return T/F
+        $sql = "UPDATE `items` SET `description` = ?, `supplier` = ?, 
+            `supplierurl` = ?, `partno` = ?, `type` = ?, `available` = ?, 
+            `picture` = ?, `realcost` = ?', `pointcost` = ?, `qty` = ?, 
+            `minqty` = ? WHERE `itemID` = ?;";
+        
+        return $this->db->query($sql, array($description, $supplier, $url, 
+            $partno, $type, $available, $picture, $realcost, $pointcost, 
+            $minqty, $itemID));
     }
     
     /**
@@ -141,6 +193,8 @@ class User_model extends CI_Model {
      */
     function modify_item_qty($itemID,$qty)
     {
+        $sql = "UPDATE `items` SET `qty` = ? WHERE `itemID` = ?;";
+        return $this->db->query($sql, array($qty, $itemID));   
     }
 
 
