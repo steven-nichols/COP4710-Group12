0ae8e6fad406575ea2c8d14166ceaa749e1419bb
Steven Nichols <steven.nichols@knights.ucf.edu>
2011-04-07 10:50:48 -0400

    Added comments to all funcitons in the style used by Kevin

--- models/login_model.php
+++ models/login_model.php
@@ -8,11 +8,22 @@ class Login_model extends CI_Model {
         $this->load->model('User_model');
         $this->load->library('session');
     }
-    
+
+    /**
+     * Returns true if the person browsing the site is a logged-in, registered 
+     * user.
+     *
+     * \return TRUE if user has been authenticated, FALSE if they have not.
+     */
     function is_logged_in(){
         return $this->session->userdata('logged_in');
     }
-     
+
+    /**
+     * Checks whether the user has already been authenticated, if so, return 
+     * immediately. Otherwise, redirect the user to the login page and loop 
+     * until they are successfully authenticated.
+     */
     function login(){
         $logged_in = is_logged_in();
 
@@ -23,7 +34,15 @@ class Login_model extends CI_Model {
     }
 
     /*
-     * Attempt to authenticate user. If sucessful, returns true.
+     * Attempt to authenticate user. If sucessful, it marks the user as 
+     * 'logged_in' and places commonly used data like the name and userid into 
+     * the user's cookie and returns true. Otherwise, return false.
+     *
+     * \param $name The name of the user used for authentication (e.i., a 
+     * username).
+     * \param $password The password the user entered on the login page.
+     * \return TRUE if the user supplied credentials match an existing user in 
+     * the database and FALSE otherwise.
      */
     function authenticate($name, $password)
     {
@@ -50,6 +69,9 @@ class Login_model extends CI_Model {
         }
     }
 
+    /**
+     * Logs the user out by deleting their session.
+     */
     function logout(){
         $this->session->sess_destroy();
     }
