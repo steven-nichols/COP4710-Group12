0d4268490328f8f4d58c048bc782d028a3f6a430
mjpellegrino <ccrunner3234@yahoo.com>
2011-04-11 16:36:23 -0700

    Added the menu body instead of separate links.

--- views/home_view.php
+++ views/home_view.php
@@ -3,11 +3,9 @@
 
 <h1>Welcome, <?php echo $first_name; ?>!</h1>
 
-<ul>
-<li><?php echo anchor('login/logout', 'Log out'); ?></li>
-<li><?php echo anchor('order', 'Place a transaction'); ?></li>
-<li><?php echo anchor('account', 'Add/modify account'); ?></li>
-</ul>
+<td>
+<?php include("menu.php"); ?>
+</td>
 
 </body>
 </html>
