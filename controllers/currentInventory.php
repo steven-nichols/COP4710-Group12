11b9d262bffc73b3244fffe2ca862c67c839ac38
mjpellegrino <ccrunner3234@yahoo.com>
2011-04-11 19:20:21 -0700

    Edited controllers/currentInventory.php via GitHub

--- controllers/currentInventory.php
+++ controllers/currentInventory.php
@@ -22,10 +22,10 @@ class currentInventory extends CI_Controller {
 		$num_items = $this->Item_model->get_item_count(1);
                 $items = $this->Item_model->get_all_items(0,$num_items,1);
                 $data = array(
-                                         "listitems" => $items
+                         "listitems" => $items
                 );
 
-		$this->load->view->('inventory', $listitems);*/
+		$this->load->view->('inventory', $listitems);
     }
 }
 ?>
