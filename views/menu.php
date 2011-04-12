be4b0dc591681465472c167c2f7b6d29406a93cb
mjpellegrino <ccrunner3234@yahoo.com>
2011-04-11 22:51:16 -0700

    yeah, didn't work like I hoped...

--- views/menu.php
+++ views/menu.php
@@ -6,38 +6,12 @@ else
 ?>" height="256" width="256" /> <br>
 
 <?php
-$this->load->model('User_model');
-$this->load->library('session');
-$userid = $this->session->userdata('userid');
-        if($this->User_model->is_trusted_helper($userid)){
-               echo anchor('login/logout', 'Log out') . '<br>';
-               echo anchor('account', 'View/Modify Profile') . '<br>';
-               echo anchor('history', 'View History') . '<br>';
-               echo anchor('shoppingCart', 'Shopping Cart') . '<br>';
-               echo anchor('mod_item', 'Add/Modify Item') . '<br>';
-               echo anchor('view_transaction', 'View Transaction History') . '<br>';
-               echo anchor('view_trans_date', 'View History of Transactions for a Date Range') . '<br>';
-               echo anchor('currentInventory', 'View Current Inventory') . '<br>';
-        }
-       
-        else if($this->User_model->is_adult_helper($userid){
-               echo anchor('login/logout', 'Log out') . '<br>';
-               echo anchor('history', 'View History') . '<br>';
-               echo anchor('shoppingCart', 'Shopping Cart') . '<br>';
-               echo anchor('view_transaction', 'View Transaction History') . '<br>';
-               echo anchor('view_trans_date', 'View History of Transactions for a Date Range') . '<br>';
-               echo anchor('currentInventory', 'View Current Inventory') . '<br>';
-       }
-
-        else if($this->User_model->is_teen_helper($userid){
-               echo anchor('login/logout', 'Log out') . '<br>';
-               echo anchor('shoppingCart', 'Shopping Cart') . '<br>';
-               echo anchor('currentInventory', 'View Current Inventory') . '<br>';
-       }
-
-        else if($this->User_model->is_child($userid){
-               echo anchor('login/logout', 'Log out') . '<br>';
-               echo anchor('shoppingCart', 'Shopping Cart') . '<br>';
-               echo anchor('currentInventory', 'View Current Inventory') . '<br>';
-       }
+echo anchor('login/logout', 'Log out') . '<br>';
+echo anchor('account', 'View/Modify Profile') . '<br>';
+echo anchor('history', 'View History') . '<br>';
+echo anchor('shoppingCart', 'Shopping Cart') . '<br>';
+echo anchor('mod_item', 'Add/Modify Item') . '<br>';
+echo anchor('view_transaction', 'View Transaction History') . '<br>';
+echo anchor('view_trans_date', 'View History of Transactions for a Date Range') . '<br>';
+echo anchor('currentInventory', 'View Current Inventory') . '<br>'; }
 ?>
